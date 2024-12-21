8eaab6293a1775683ade6f37f2d8766ec5b541d0
Kenny Siwawa Chikwama <kenny.chikwama@gmail.com>
2024-12-21 17:27:26 +0000

    minitalk

---
+++ server.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchikwam <kchikwam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/21 18:17:59 by kchikwam          #+#    #+#             */
+/*   Updated: 2024/12/21 18:18:03 by kchikwam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf/ft_printf.h"
+
+void	bit_handler(int bit)
+{
+	int	i;
+
+	i = 0;
+	g_msg.c += ((bit & 1) << g_msg.i);
+	g_msg.i++;
+	if (g_msg.i == 7)
+	{
+		ft_printf("%c", g_msg.c);
+		if (!g_msg.c)
+			ft_printf("\n");
+		g_msg.c = 0;
+		g_msg.i = 0;
+	}
+}
+
+int	main(void)
+{
+	ft_printf("Welcome To Pasquale's Server!\n");
+	ft_printf("My Server PID is: %d\n", getpid());
+	while (1)
+	{
+		signal(SIGUSR2, bit_handler);
+		signal(SIGUSR1, bit_handler);
+		pause();
+	}
+	return (0);
+}
