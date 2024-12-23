bd8677db95460c6a3d6494f18ddcf6d814b95812
Kenny Siwawa Chikwama <kenny.chikwama@gmail.com>
2024-12-23 14:15:37 +0000

    minitalk

--- client.c
+++ client.c
@@ -1,3 +1,4 @@
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -14,40 +15,60 @@
 #include "ft_printf/libft/libft.h"
 #include <unistd.h>
 #include <signal.h>
+#include <stdio.h> // Include the standard I/O library for debugging
 
-void	send_bit(int pid, char *str, size_t len)
+void send_bit(int pid, char *str, size_t len)
 {
-	int		shift;
-	size_t	i;
+    int		shift;
+    size_t	i;
 
-	i = 0;
-	while (i <= len)
-	{
-		shift = 0;
-		while (shift < 8) //changed from 7 to 8
-		{
-			if ((str[i] >> shift) & 1)
-				kill(pid, SIGUSR2);
-			else
-				kill(pid, SIGUSR1);
-			shift++;
-			usleep(300);
-		}
-		i++;
-	}
+    i = 0;
+    while (i <= len)
+    {
+        shift = 0;
+        while (shift < 8) //changed from 7 to 8
+        {
+            if ((str[i] >> shift) & 1)
+            {
+                if (kill(pid, SIGUSR2) == -1)
+                {
+                    ft_printf("Error sending SIGUSR2\n");
+                    return;
+                }
+				printf("Sending SIGUSR2 for bit 1\n"); // Debug print
+            }
+            else
+            {
+                if (kill(pid, SIGUSR1) == -1)
+                {
+                    ft_printf("Error sending SIGUSR1\n");
+                    return;
+                }
+				printf("Sending SIGUSR1 for bit 0\n"); // Debug print
+            }
+            shift++;
+            usleep(300);
+        }
+        i++;
+    }
 }
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	int		pid;
-	char	*str;
+    int		pid;
+    char	*str;
 
-	if (argc == 3)
-	{
-		pid = ft_atoi(argv[1]);
-		str = argv[2];
-		send_bit(pid, str, ft_strlen(str));
-	}
-	else
-		ft_printf("\nYOU EITHER LEFT IT BLANK OR ARE DOING MORE THAN 1 WORD\n\n");
+    if (argc == 3)
+    {
+        pid = ft_atoi(argv[1]);
+        str = argv[2];
+        send_bit(pid, str, ft_strlen(str));
+        // Send the null terminator
+        send_bit(pid, "\0", 1);
+    }
+    else
+    {
+        ft_printf("\nUsage: %s <PID> <message>\n", argv[0]);
+    }
+    return 0;
 }
--- server.c
+++ server.c
@@ -6,54 +6,15 @@
 /*   By: kchikwam <kchikwam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 18:17:59 by kchikwam          #+#    #+#             */
-/*   Updated: 2024/12/23 13:04:38 by kchikwam         ###   ########.fr       */
+/*   Updated: 2024/12/23 14:09:44 by kchikwam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// #include "ft_printf/ft_printf.h"
-// #include "ft_printf/libft/libft.h"
-// #include <signal.h>
-// #include <unistd.h>
-
-// typedef struct s_msg
-// {
-// 	char	c;
-// 	int		i;
-// }	t_msg;
-
-// t_msg g_msg = {0, 0};
-
-// void	bit_handler(int bit)
-// {
-// 	g_msg.c += ((bit & 1) << g_msg.i);
-// 	g_msg.i++;
-// 	if (g_msg.i == 7)
-// 	{
-// 		ft_printf("%c", g_msg.c);
-// 		if (!g_msg.c)
-// 			ft_printf("\n");
-// 		g_msg.c = 0;
-// 		g_msg.i = 0;
-// 	}
-// }
-
-// int	main(void)
-// {
-// 	ft_printf("Welcome To Pasquale's Server!\n");
-// 	ft_printf("My Server PID is: %d\n", getpid());
-// 	while (1)
-// 	{
-// 		signal(SIGUSR2, bit_handler);
-// 		signal(SIGUSR1, bit_handler);
-// 		pause();
-// 	}
-// 	return (0);
-// }
 
 #include "ft_printf/ft_printf.h"
 #include "ft_printf/libft/libft.h"
 #include <signal.h>
 #include <unistd.h>
+#include <stdio.h> // Include the standard I/O library for debugging
 
 typedef struct s_msg
 {
@@ -62,13 +23,15 @@ typedef struct s_msg
 }	t_msg;
 
 t_msg g_msg = {0, 0};
-// volatile sig_atomic_t g_running = 1;
+volatile sig_atomic_t g_running = 1;
 
 void	bit_handler(int bit)
 {
+    printf("Signal received: %d\n", bit); // Debug print
     g_msg.c += ((bit & 1) << g_msg.i);
     g_msg.i++;
-    if (g_msg.i == 8) // changed from 7 to 8
+    printf("Current char: %c, bit position: %d\n", g_msg.c, g_msg.i); // Debug print
+    if (g_msg.i == 8) //Adjust to 8 bits for a full character
     {
         ft_printf("%c", g_msg.c);
         if (!g_msg.c)
@@ -78,21 +41,27 @@ void	bit_handler(int bit)
     }
 }
 
-//void	sigint_handler(int signum)
-//{
-//    (void)signum;
-//    g_running = 0;
-//}
+void	sigint_handler(int signum)
+{
+    (void)signum;
+    printf("SIGINT received. Shutting down...\n"); // Debug print
+    g_running = 0;
+}
 
 int	main(void)
 {
-    ft_printf("Welcome To Kenny's Server!\n");
+    ft_printf("Welcome To Pasquale's Server!\n");
     ft_printf("My Server PID is: %d\n", getpid());
-    while (1)
+
+    signal(SIGUSR2, bit_handler);
+    signal(SIGUSR1, bit_handler);
+    signal(SIGINT, sigint_handler);
+
+    while (g_running)
     {
-        signal(SIGUSR2, bit_handler);
-        signal(SIGUSR1, bit_handler);
         pause();
     }
+
+    ft_printf("Server shutting down...\n");
     return (0);
 }
